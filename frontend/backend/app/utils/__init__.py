bff5a20c25702a0e5499eb3d7b7b5b4120006989
Muhammad Abdullah <m.abdullahazhar070@gmail.com>
2026-02-08 17:45:59 +0500

    Delete frontend/backend directory

--- frontend/backend/app/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Utils package for AI Study Companion
-"""
-from app.utils.helpers import format_duration, truncate_text, calculate_reading_time
-
-__all__ = ['format_duration', 'truncate_text', 'calculate_reading_time']
